83958f620c6c180d59fed1e0ab70c13230707daa
Nelson Alves <nelsonayamashita@gmail.com>
2020-08-22 20:12:07 -0300

    Adiciona algoritmos

---
+++ Aprendizado por Reforço Clássico/bandit.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+class GaussianBandit(object):
+
+    def __init__(self, k_bracos=10):
+        self.bandits_expectations = np.random.normal(0,1,k_bracos)
+    
+    def gamble(self, action):
+        """ação(int) -> recompensa(int)
+        Recebe uma ação representando o bandit que será acionado, que devolve uma recompensa baseada em uma distribuição normal de desvio padrão 1 e média 0.
+        """
+        return np.random.normal(self.bandits_expectations[action], 1)
